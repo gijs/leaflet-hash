49ecf8da4ad59463162ad581487a372703af3cb1
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-11-30 08:36:35 -0500

    works

--- leaflet-hash.js
+++ leaflet-hash.js
@@ -18,6 +18,7 @@
     L.Hash.prototype = {
         map: null,
         lastHash: null,
+        lc: null,
     
         parseHash: function(hash) {
             if(hash.indexOf('#') == 0) {
@@ -30,7 +31,17 @@
                     lon = parseFloat(args[2]);
                 if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
                     return false;
+                } else if (args.length == 3){
+                    return {
+                        center: new L.LatLng(lat, lon),
+                        zoom: zoom
+                    };
                 } else {
+                    if(args.length == 4){
+                        this.parseLayer(args[3]);
+                    } else{
+                        this.parseLayer(args[3], args[4]);
+                    }
                     return {
                         center: new L.LatLng(lat, lon),
                         zoom: zoom
@@ -69,6 +80,7 @@
             // reset the hash
             this.lastHash = null;
             this.onHashChange();
+            this.update();
     
             if (!this.isListening) {
                 this.startListening();
@@ -101,19 +113,19 @@
         update: function() {
             var hash = location.hash;
             if (hash === this.lastHash) {
-                // console.info("(no change)");
+                console.info(hash+"!"+this.lastHash);
                 return;
             }
             var parsed = this.parseHash(hash);
             if (parsed) {
-                // console.log("parsed:", parsed.zoom, parsed.center.toString());
+                console.log("parsed:", parsed.zoom, parsed.center.toString());
                 this.movingMap = true;
                 
                 this.map.setView(parsed.center, parsed.zoom);
                 
                 this.movingMap = false;
             } else {
-                // console.warn("parse error; resetting:", this.map.getCenter(), this.map.getZoom());
+                console.warn("parse error; resetting:", this.map.getCenter(), this.map.getZoom());
                 this.onMapMove(this.map);
             }
         },
@@ -171,12 +183,64 @@
 				overlay: overlay,
 				base: base[0]
 			};
+		},
+		parseLayer: function(base, over){
+			var bName = decodeURIComponent(base);
+			if(over){
+				this.parseOver(over);
+			}
+			var len = lc._baseLayersList.childNodes.length;
+			for(var i = 0; i<len; i++){
+					eachNode(lc._baseLayersList.childNodes[i]);
+			}
+				function eachNode(v){
+				var parts = v.childNodes;
+				var name = parts[1].data.slice(1)
+				if(bName === name){
+					parts[0].setAttribute("checked", "checked")
+				}
+			}
+			lc._onInputClick();
+		},
+		parseOver: function(over){
+			if(over.indexOf("-")>=0){
+			var oArray = over.split("-");
+			}else{
+				var oArray = [over]
+			}
+			var lc = this.lc;
+			oArray.forEach(function(oPart){
+				var bName = decodeURIComponent(oPart);
+				var len = lc._overlaysList.childNodes.length;
+				for(var i = 0; i<len; i++){
+					eachNode(lc._overlaysList.childNodes[i]);
+				}
+				function eachNode(v){
+				var parts = v.childNodes;
+				var name = parts[1].data.slice(1)
+				if(bName === name){
+					parts[0].checked = true;
+				}else{
+					parts[0].checked = false;
+				}
+			}
+			});
+			lc._onInputClick();
 		}
     };
-    L.hash = function(map){
-        return new L.Hash(map);	
+    L.hash = function(map, lc){
+    	if(lc){
+    		return new L.Hash(map, lc);
+    	}else{
+    		return new L.Hash(map);
+    	}
+        	
     };
-    L.Map.prototype.addHash = function(){
+    L.Map.prototype.addHash = function(lc){
+    	if(lc){
+    		L.hash(this, lc);
+    	}else{
 		L.hash(this);
+    	}
 	};
 })(window);
